468e6ba6e84b6015354f3f73f5a7b865b0a14f8d
Vinh <hoangzinhvn@gmail.com>
2017-07-25 11:48:17 +0700

    Add production=false when run yarn install

--- lib/dependencies.sh
+++ lib/dependencies.sh
@@ -76,9 +76,9 @@ yarn_node_modules() {
   echo "Installing node modules (yarn.lock)"
   cd "$build_dir"
   if yarn_supports_frozen_lockfile; then
-    yarn install --frozen-lockfile --ignore-engines 2>&1
+    yarn install --production=false --frozen-lockfile --ignore-engines 2>&1
   else
-    yarn install --pure-lockfile --ignore-engines 2>&1
+    yarn install --production=false --pure-lockfile --ignore-engines 2>&1
   fi
 }
 
